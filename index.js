2d1bfc95ced7944e1c5805a93cb6501b070e2cfe
Alis Adrović <alis.adrovic@gmail.com>
2023-09-15 22:50:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const { fromTemporaryCredentials } = require("@aws-sdk/credential-providers");
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+
+const region = process.env.AWS_REGION;
+const roleArn = process.env.AWS_ROLE_ARN;
+const roleSessionName = process.env.AWS_ROLE_SESSION_NAME;
+const parameterStoreName = process.env.AWS_PARAMETER_STORE_NAME;
+
+// The default implicit credentials provider is NodeProviderChain:
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-credential-providers/#fromNodeProviderChain
+// which gives precedence to environment variables as needed in our use-case:
+// - AWS_PROFILE env variable (in local dev environment)
+// - and then to AWS_WEB_IDENTITY_TOKEN_FILE (in K8s pod)
+const client = new SSMClient({
+  region,
+  credentials: fromTemporaryCredentials({ // assume role, as SSM is in different account
+    params: {
+      RoleArn: roleArn,
+      RoleSessionName: roleSessionName
+    },
+    clientConfig: { region }
+  })
+});
+
+const input = {
+  Name: parameterStoreName,
+  WithDecryption: true
+};
+const command = new GetParameterCommand(input);
+
+(async () => {
+  try {
+    const response = await client.send(command);
+  
+    console.log(JSON.parse(response.Parameter.Value));
+  } catch (err) {
+    console.error(err)
+  }
+})()
